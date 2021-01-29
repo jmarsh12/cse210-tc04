540bc11825f5cc9f3c8899c26d69b58b4891cc26
jmarsh12 <jeffrey.marsh56@gmail.com>
2021-01-29 15:19:07 -0700

    added skeleton functions

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -1,2 +1,17 @@
 class Dealer:
+
+    def __init__(self):
+        pass
+
+    def draw_card(self):
+        # TODO pick a random card between 1 and 13, returns that card value
+        # TODO have a for loop to loop between cards (for i in range(1, 14))
+        pass
+
+    def determine_points(self):
+        # TODO pass draw_card value into determine points, return number of points (if and else statement)
+        # TODO get_last_card, is_last_card_higher, is_last_card_lower
+        # TODO if correct, add 100 points, if incorrect lose 75 points
+        pass
+
     pass
--- hilo/game/director.py
+++ hilo/game/director.py
@@ -1,2 +1,22 @@
 class Director:
+
+    def __init__(self):
+        pass
+
+    def start_game(self):
+        # TODO get inputs(higher/lower), do updates, do outputs
+        pass
+
+    def points(self):
+        # TODO compute points/track points
+        pass
+
+    def play_again(self):
+        # TODO ask to play again, determine if the player can play again
+        pass
+
+    def get_choice(self):
+        # TODO get user input 
+        # TODO figure out how to connect if the user's number is higher or lower than the card value
+        pass
     pass
