4f86b38aa49c78c0b3a1f7abdfc0958257fa3054
purnsup <77720252+purnsup@users.noreply.github.com>
2021-02-01 13:31:55 -0700

    first commit

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -1,16 +1,21 @@
+import random
+
 class Dealer:
     '''
     
     '''
 
     def __init__(self):
-
+        self.card = []
+        self.num_draw = 0
+    
     def draw_card(self):
         # TODO pick a random card between 1 and 13, returns that card value
         # TODO have a for loop to loop between cards (for i in range(1, 14))
+        result = random.randing(1, 13)
+        self.card.append(result)
+        self.num_draw += 1
         
-        
-
     def determine_points(self):
         # TODO pass draw_card value into determine points, return number of points (if and else statement)
         # TODO get_last_card, is_last_card_higher, is_last_card_lower
