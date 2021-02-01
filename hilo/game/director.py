c8f0816fa0868c1bc2b6c2359a8ed748316c9d78
Bryson <bryson.berrett19@gmail.com>
2021-02-01 13:54:40 -0700

    director modified

--- hilo/game/director.py
+++ hilo/game/director.py
@@ -7,21 +7,22 @@ class Director:
     '''
 
     def __init__(self):
-        self.points = 0
-        pass
     
-    def points(self, choice):
+
+    def start_game(self):
+        # TODO get inputs(higher/lower), do updates, do outputs
+        print('hi')
+        play_again()
+        
+
+    def points(self):
         # TODO compute points/track points
-        if choice == 'h' or choice == 'H':
-            self.points += 100
-        else:
-            self.points -= 75
         pass
 
     def play_again(self):
         # TODO ask to play again, determine if the player can play again
         play_again = input("Do you want to play again? y/n")
-        if play_again == "y":
+        if play_again == "y"
             get_choice()
         else:
             exit()
@@ -29,9 +30,14 @@ class Director:
 
     def get_choice(self):
         print('The card is {card}')
+        condition = True
+        while(condition)
         choice = input("Higher or Lower [h/l]")
-        if choice == 'h' or choice == 'H':
+        if choice == 'h' or choice == 'H' or choice == 'L' or choice == 'l':
+            condition = False
             return choice
+        else:
+            print("Please choose an h/H or an l/L")
         # TODO get user input 
         # TODO figure out how to connect if the user's number is higher or lower than the card value
         pass
